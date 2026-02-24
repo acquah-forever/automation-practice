505af4f533ee3de5046b5877347709ccbe5a9d60
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-02-24 18:45:01 +0000

    formatting FAQ's

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,29 +1,78 @@
 import React, { useState } from 'react'
-import { ChevronDown,ChevronUp } from 'lucide-react'
+import { ChevronDown, ChevronUp } from 'lucide-react'
 
 const Faq = () => {
     const [open, setOpen] = useState(false)
-    function handleClick(index){
+    function handleClick(index) {
         setOpen(prev => (prev === index ? null : index))
     }
     return (
-        <div>
-            <div>
+        <div className='mt-30 flex flex-col justify-center items-center'>
+            <div className='text-center space-y-4 mb-4'>
                 <h1 className="text-3xl font-semibold">FAQ's</h1>
                 <h3 className="font-light text-sm">Looking for answers to your frquently asked questions? Check
                     <br />
                     out our FAQ's section below. </h3>
             </div>
-
-            <div className='bg-slate-300 p-3 mb-1 rounded-lg max-w-xl flex justify-between items-center'onClick={() => handleClick(1)}>
-                <h1>Do I need coding or design experince to use kojo?</h1>
-                {open === 1 ? (<ChevronUp />) : (<ChevronDown />)}
+            
+            <div>
+                <div className='bg-slate-300 p-3 mb-1 rounded-lg max-w-xl flex justify-between items-center' onClick={() => handleClick(1)}>
+                    <h1>Do I need coding or design experince to use kojo?</h1>
+                    {open === 1 ? (<ChevronUp />) : (<ChevronDown />)}
+                </div>
+                {open === 1 && (
+                    <h1>Basic coding knowledge (HTML/CSS, Tailwind) helps, but advanced design
+                        <br />
+                        skills aren't required. You can use components as-is or customize them.</h1>
+                )}
             </div>
-            {open === 1 && (
-                <h1>Basic coding knowledge (HTML/CSS, Tailwind) helps, but advanced design
-                    <br />
-                    skills aren't required. You can use components as-is or customize them.</h1>
-            )}
+
+            <div>
+                <div className='bg-slate-300 p-3 mb-1 rounded-lg max-w-xl flex justify-between items-center' onClick={() => handleClick(2)}>
+                    <h1>Wshat is kojo and how does it helps developers?</h1>
+                    {open === 2 ? (<ChevronUp />) : (<ChevronDown />)}
+                </div>
+                {open === 2 && (
+                    <h1>kojo provides ready-to-use, customizable UI components. and
+                        <br />
+                        templates, saving time fro developers.</h1>
+                )}
+            </div>
+
+            <div>
+                <div className='bg-slate-300 p-3 mb-1 rounded-lg max-w-xl flex justify-between items-center' onClick={() => handleClick(3)}>
+                    <h1>Can I use kojo components in my existing project?</h1>
+                    {open === 3 ? (<ChevronUp />) : (<ChevronDown />)}
+                </div>
+                {open === 3 && (
+                    <h1>Yes, components can be intergrated with HTML, React and Next.js, Vue
+                        <br />
+                        and other projects using Tailwind CSS.</h1>
+                )}
+            </div>
+
+            <div>
+                <div className='bg-slate-300 p-3 mb-1 rounded-lg max-w-xl flex justify-between items-center' onClick={() => handleClick(4)}>
+                    <h1>Does kojo suppot team collaboration?</h1>
+                    {open === 4 ? (<ChevronUp />) : (<ChevronDown />)}
+                </div>
+                {open === 4 && (
+                    <h1>There is no clear documentation on built-in collaboration features. Check
+                        <br />
+                        the support system for details.</h1>
+                )}
+            </div>
+
+            <div>
+                <div className='bg-slate-300 p-3 mb-1 rounded-lg max-w-xl flex justify-between items-center' onClick={() => handleClick(5)}>
+                    <h1>Can I kojo before purchasig a plan?</h1>
+                    {open === 5 ? (<ChevronUp />) : (<ChevronDown />)}
+                </div>
+                {open === 5 && (
+                    <h1>Yes. You can try kojo with full access to all the features.</h1>
+                )}
+            </div>
+
         </div>
     )
 }
